fe0a66fa5d49040409fa6fb564bcb1f7db8deb70
Jarrett Ye <jarrett.ye@outlook.com>
2023-04-16 17:40:09 +0800

    add Unordered Lists

--- stats.py
+++ stats.py
@@ -5,7 +5,7 @@ from .utils import *
 todayStats_old = anki.stats.CollectionStats.todayStats
 
 
-def _line_now(self, i, a, b, bold=True):
+def _line_now(i, a, b, bold=True):
     colon = _(":")
     if bold:
         i.append(("<tr><td align=right>%s%s</td><td><b>%s</b></td></tr>") % (a,colon,b))
@@ -13,7 +13,7 @@ def _line_now(self, i, a, b, bold=True):
         i.append(("<tr><td align=right>%s%s</td><td>%s</td></tr>") % (a,colon,b))
 
 
-def _lineTbl_now(self, i):
+def _lineTbl_now(i):
     return "<table>" + "".join(i) + "</table>"
 
 
@@ -32,22 +32,16 @@ def retention_stability_burden() -> float:
 def todayStats_new(self):
     i = []
     retention, stability, burden, count = retention_stability_burden()
-    _line_now(self, i, "Average retention", f"{retention * 100: .2f}%")
-    _line_now(self, i, "Average stability", f"{int(stability)} days")
-    _line_now(self, i, "Total burden", f"{burden: .2f} reviews")
-    _line_now(self, i, "Count", f"{count} cards")
-    stats_data = _lineTbl_now(self, i)
-    # i = []
-    # _line_now(self, i, "Retention", "0.9 ^ elapsed days / stability")
-    # _line_now(self, i, "Stability", "elapsed days when retention = 90%")
-    # _line_now(self, i, "Burden", "1 / interval")
-    # _line_now(self, i, "Count", "number of cards with custom data")
-    # stats_info = _lineTbl_now(self, i)
+    _line_now(i, "Average retention", f"{retention * 100: .2f}%")
+    _line_now(i, "Average stability", f"{int(stability)} days")
+    _line_now(i, "Total burden", f"{burden: .2f} reviews/day")
+    _line_now(i, "Count", f"{count} cards")
+    stats_data = _lineTbl_now(i)
     interpretation = "<h3>Interpretation</h3>" \
-        + "Retention: the probability of recalling a card today. The average retention is often higher than your requested retention because it  is calculated in all cards including those undue cards. <br>" \
-        + "Stability: the number of days it takes for the retention to decay from 100% to 90%. <br>" \
-        + "Burden: the number of reviews needed to maintain retention. Total burden is the sum of reciprocals of all intervals, in days (aka the sum of 1/interval, like 1/I<sub>1</sub> + 1/I<sub>2</sub> + 1/I<sub>3</sub> +...+ 1/I<sub>n</sub>). <br>" \
-        + "Count: the number of cards with custom data. <br>"
+        + "<ul><li>Retention: the probability of recalling a card today. The average retention is often higher than your requested retention because it  is calculated in all cards including those undue cards. </li>" \
+        + "<li>Stability: the number of days it takes for the retention to decay from 100% to 90%. </li>" \
+        + "<li>Burden: the number of reviews needed to maintain retention perday. Total burden is the sum of reciprocals of all intervals, in days (aka the sum of 1/interval, like 1/I<sub>1</sub> + 1/I<sub>2</sub> + 1/I<sub>3</sub> +...+ 1/I<sub>n</sub>). </li>" \
+        + "<li>Count: the number of cards with custom data. </li></ul>"
     return todayStats_old(self) + "<br><br><table style='text-align: center'><tr><td style='padding: 5px'>" \
         + "<h2>FSRS Stats</h2>" + stats_data + "</td></tr></table>" \
         + "<table style='text-align: left'><tr><td style='padding: 5px'>" + interpretation + "</td></tr></table>"
