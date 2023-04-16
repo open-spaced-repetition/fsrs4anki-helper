1c81ecb3ff888e76b99d16376df85ba19733a03b
Jarrett Ye <jarrett.ye@outlook.com>
2023-04-16 12:17:09 +0800

    add interpretation & consider (re)learn card

--- stats.py
+++ stats.py
@@ -19,9 +19,9 @@ def _lineTbl_now(self, i):
 
 def retention_stability_burden() -> float:
     today = mw.col.sched.today
-    last_due_and_custom_data_and_ivl_rows = mw.col.db.all("select due - ivl, data, ivl from cards where queue = 2 and data like '%\"cd\"%'")
-    delay_stability_ivl_list = map(lambda x: (today - x[0], json.loads(json.loads(x[1])['cd'])['s'], x[2]), last_due_and_custom_data_and_ivl_rows)
-    retention_stability_burden_list = list(map(lambda x: (math.pow(0.9, x[0] / x[1]), x[1], 1/x[2]), delay_stability_ivl_list))
+    last_due_and_custom_data_and_ivl_rows = mw.col.db.all("select due - ivl, data, ivl from cards where queue >= 1 and data like '%\"cd\"%'")
+    delay_stability_ivl_list = map(lambda x: (today - min(today, x[0]), json.loads(json.loads(x[1])['cd'])['s'], x[2]), last_due_and_custom_data_and_ivl_rows)
+    retention_stability_burden_list = list(map(lambda x: (math.pow(0.9, x[0] / x[1]), x[1], 1/max(1, x[2])), delay_stability_ivl_list))
     recall_sum = sum(item[0] for item in retention_stability_burden_list)
     stability_sum = sum(item[1] for item in retention_stability_burden_list)
     burden_sum = sum(item[2] for item in retention_stability_burden_list)
@@ -37,15 +37,20 @@ def todayStats_new(self):
     _line_now(self, i, "Total burden", f"{burden: .2f} reviews")
     _line_now(self, i, "Count", f"{count} cards")
     stats_data = _lineTbl_now(self, i)
-    i = []
-    _line_now(self, i, "Retention", "0.9 ^ elapsed days / stability")
-    _line_now(self, i, "Stability", "elapsed days when retention = 90%")
-    _line_now(self, i, "Burden", "1 / interval")
-    _line_now(self, i, "Count", "number of cards with custom data")
-    stats_info = _lineTbl_now(self, i)
+    # i = []
+    # _line_now(self, i, "Retention", "0.9 ^ elapsed days / stability")
+    # _line_now(self, i, "Stability", "elapsed days when retention = 90%")
+    # _line_now(self, i, "Burden", "1 / interval")
+    # _line_now(self, i, "Count", "number of cards with custom data")
+    # stats_info = _lineTbl_now(self, i)
+    interpretation = "<h3>Interpretation</h3>" \
+        + "Retention: the probability of recalling a card today. The average retention is often higher than your requested retention because it  is calculated in all cards including those undue cards. <br>" \
+        + "Stability: the number of days it takes for the retention to decay from 100% to 90%. <br>" \
+        + "Burden: the number of reviews needed to maintain retention. Total burden is the sum of reciprocals of all intervals, in days (aka the sum of 1/interval, like 1/I<sub>1</sub> + 1/I<sub>2</sub> + 1/I<sub>3</sub> +...+ 1/I<sub>n</sub>). <br>" \
+        + "Count: the number of cards with custom data. <br>"
     return todayStats_old(self) + "<br><br><table style='text-align: center'><tr><td style='padding: 5px'>" \
-        + "<h2>FSRS Stats</h2>" + stats_data + "</td><td style='padding: 5px'>" \
-        + "<h2>Stats Info</h2>" + stats_info + "</td></tr></table>"
+        + "<h2>FSRS Stats</h2>" + stats_data + "</td></tr></table>" \
+        + "<table style='text-align: left'><tr><td style='padding: 5px'>" + interpretation + "</td></tr></table>"
 
 
 def init_stats():
