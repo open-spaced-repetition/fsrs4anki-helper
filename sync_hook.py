7dab2cc6a0c721980ecdb0b88b7b0306e57da04b
Jarrett Ye <jarrett.ye@outlook.com>
2023-04-18 18:03:09 +0800

    rename variables

--- sync_hook.py
+++ sync_hook.py
@@ -6,14 +6,14 @@ from .configuration import Config
 from .utils import *
 
 
-def create_comparelog(old_rids: List[int]) -> None:
-    old_rids.extend(
+def create_comparelog(local_rids: List[int]) -> None:
+    local_rids.extend(
         [id for id in mw.col.db.list("SELECT id FROM revlog")]
     )
 
 
-def auto_reschedule(old_rids: List[int]):
-    if len(old_rids) == 0:
+def auto_reschedule(local_rids: List[int]):
+    if len(local_rids) == 0:
         return
     config = Config()
     config.load()
@@ -21,20 +21,21 @@ def auto_reschedule(old_rids: List[int]):
         return
     
 
-    old_rid_string = ",".join([str(old_rid) for old_rid in old_rids])
+    local_rid_string = ",".join([str(local_rid) for local_rid in local_rids])
 
     # exclude entries where ivl == lastIvl: they indicate a dynamic deck without rescheduling
-    reviewed_cids = [cid for cid in mw.col.db.list(
-            f"SELECT DISTINCT cid FROM revlog WHERE id NOT IN ({old_rid_string}) and ivl != lastIvl"
+    remote_reviewed_cids = [cid for cid in mw.col.db.list(
+            f"SELECT DISTINCT cid FROM revlog WHERE id NOT IN ({local_rid_string}) and ivl != lastIvl"
         )
     ]
 
-    reschedule(None, recent=False, filter=True, filtered_cids=set(reviewed_cids))
-    cid_string = ",".join([str(cid) for cid in reviewed_cids])
-    rescheduled_nids_with_siblings = [nid for nid in mw.col.db.list(
+    reschedule(None, recent=False, filter=True, filtered_cids=set(remote_reviewed_cids))
+
+    remote_reviewed_cid_string = ",".join([str(cid) for cid in remote_reviewed_cids])
+    rescheduled_nids_have_siblings = [nid for nid in mw.col.db.list(
         f"""SELECT DISTINCT nid 
             FROM cards 
-            WHERE id IN ({cid_string}) 
+            WHERE id IN ({remote_reviewed_cid_string}) 
             and queue = 2 
             and nid IN (
                 SELECT nid
@@ -46,13 +47,13 @@ def auto_reschedule(old_rids: List[int]):
             )"""
         )
     ]
-    affected_notes = len(rescheduled_nids_with_siblings)
+    affected_notes = len(rescheduled_nids_have_siblings)
     if affected_notes > 0 and askUser(f"Rescheduling done. {affected_notes} notes with siblings affected. Disperse siblings?"):
-        disperse_siblings(None, filter=True, filtered_nid_string=",".join([str(nid) for nid in rescheduled_nids_with_siblings]))
+        disperse_siblings(None, filter=True, filtered_nid_string=",".join([str(nid) for nid in rescheduled_nids_have_siblings]))
 
 
 def init_sync_hook():
-    old_rids = []
+    local_rids = []
 
-    sync_will_start.append(lambda: create_comparelog(old_rids))
-    sync_did_finish.append(lambda: auto_reschedule(old_rids))
+    sync_will_start.append(lambda: create_comparelog(local_rids))
+    sync_did_finish.append(lambda: auto_reschedule(local_rids))
