ecf7b8b4db08ed3ed25fa47df3edf82cba77598a
Luc Mcgrady <lucmcgrady@gmail.com>
2023-05-01 20:00:35 +0100

    Added: Critical fail message + name to temp file

--- optimizer.py
+++ optimizer.py
@@ -1,8 +1,9 @@
 from .utils import *
 
 from anki.exporting import AnkiPackageExporter
+from anki.decks import DeckManager
 from aqt.qt import QProcess
-from aqt.utils import showInfo
+from aqt.utils import showInfo, showCritical
 
 import os
 import sys
@@ -26,34 +27,48 @@ class ExclusiveWorker:
             self.process.start(args[0], args[1:])
             self.process.finished.connect(wrapper)
             self.working = True
-            
+
             tooltip(self.message)
         else:
             tooltip(f"Waiting for '{self.message}' to complete")
 
 _worker = ExclusiveWorker()
-install_checked = False
+
+
 
 def optimize(did: int):
     global _worker
 
     try:
-        from fsrs4anki_optimizer import Optimizer
+        from fsrs4anki_optimizer import Optimizer # only used as a check to see if its installed
     except ImportError:
-        showWarning("You need to have the optimizer installed in order to optimize your decks using this option")
+        showCritical(
+"""
+You need to have the optimizer installed in order to optimize your decks using this option.
+Please run Tools>FSRS4Anki helper>Install local optimizer.
+Alternatively, use a different method of optimizing (https://github.com/open-spaced-repetition/fsrs4anki/releases)
+""")
         return
 
     exporter = AnkiPackageExporter(mw.col)
+    manager = DeckManager(mw.col)
+    deck = manager.get(did)
+    assert deck
+    name = deck["name"]
+
+    dir_path = os.path.expanduser("~/.fsrs4ankiHelper")
+    tmp_dir_path = f"{dir_path}/tmp"
 
     exporter.did = did
     exporter.includeMedia = False
     exporter.includeSched = True
+
+    filepath = f"{tmp_dir_path}/{name}.apkg"
     
-    dirpath = os.path.expanduser("~/.fsrs4ankiHelperTemp")
-    filepath = f"{dirpath}/{did}.apkg"
-    
-    if not os.path.isdir(dirpath):
-        os.mkdir(dirpath)
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+    if not os.path.isdir(tmp_dir_path):
+        os.mkdir(tmp_dir_path)
 
     exporter.exportInto(filepath) 
 
@@ -78,10 +93,14 @@ def optimize(did: int):
 
     out_save = os.path.expanduser("~/fsrs4ankioptimized")
 
-    def optimize():
-        _worker.work([sys.executable, "-m", "fsrs4anki_optimizer", filepath, "-y", "-o", out_save])
+    def on_complete():
+        showInfo(f"Optimized parameters saved at {out_save}")
 
-    _worker.work(optimize)
+    _worker.work(
+        [sys.executable, "-m", "fsrs4anki_optimizer", filepath, "-y", "-o", out_save],
+        on_complete,
+        f"Optimizing {name}"
+    )
 
     #tooltip(f"Parameters saved at \"{out_save}\"")
 
