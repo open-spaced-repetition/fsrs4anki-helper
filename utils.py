131f39c3b9993d6b60af445ca1e04efd766033d2
Jarrett Ye <jarrett.ye@outlook.com>
2023-05-28 21:37:31 +0800

    Fix/+1 day for resetting cards

--- utils.py
+++ utils.py
@@ -187,7 +187,7 @@ def RepresentsInt(s):
 def reset_ivl_and_due(cid: int, revlogs: List[RevlogEntry]):
     card = mw.col.get_card(cid)
     card.ivl = int(revlogs[0].interval / 86400)
-    due = int(round((revlogs[0].time + revlogs[0].interval - mw.col.sched.day_cutoff) / 86400) + mw.col.sched.today)
+    due = int(round((revlogs[0].time + revlogs[0].interval - mw.col.sched.day_cutoff + 1) / 86400) + mw.col.sched.today)
     if card.odid:
         card.odue = due
     else:
