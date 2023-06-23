5f6dabed01f21cf525bb803d0d18b27d359f13a5
Jarrett Ye <jarrett.ye@outlook.com>
2023-06-23 17:41:16 +0800

    ensure fuzz range >= due when due > today

--- disperse_siblings.py
+++ disperse_siblings.py
@@ -18,10 +18,11 @@ def get_siblings(did=None, filter_flag=False, filtered_nid_string=""):
         did_list = ids2str(mw.col.decks.deck_and_child_ids(did))
     siblings = mw.col.db.all(f"""
     SELECT 
-        id, 
-        nid, 
+        id,
+        nid,
         did,
-        json_extract(json_extract(IIF(data != '', data, NULL), '$.cd'), '$.s')
+        json_extract(json_extract(IIF(data != '', data, NULL), '$.cd'), '$.s'),
+        CASE WHEN odid==0 THEN due ELSE odue END
     FROM cards
     WHERE nid IN (
         SELECT nid
@@ -39,14 +40,14 @@ def get_siblings(did=None, filter_flag=False, filtered_nid_string=""):
     {"AND did IN %s" % did_list if did is not None else ""}
     """)
     siblings = filter(lambda x: x[3] is not None, siblings)
-    siblings_dict = {}
-    for cid, nid, did, stability in siblings:
-        if nid not in siblings_dict:
-            siblings_dict[nid] = []
-        siblings_dict[nid].append((cid, did, stability))
-    return siblings_dict
+    nid_siblings_dict = {}
+    for cid, nid, did, stability, due in siblings:
+        if nid not in nid_siblings_dict:
+            nid_siblings_dict[nid] = []
+        nid_siblings_dict[nid].append((cid, did, stability, due))
+    return nid_siblings_dict
 
-def get_due_range(cid, parameters, stability):
+def get_due_range(cid, parameters, stability, due):
     revlogs = filter_revlogs(mw.col.card_stats_data(cid).revlog)
     last_due = get_last_review_date(revlogs[0])
     last_rating = revlogs[0].button_chosen
@@ -55,18 +56,19 @@ def get_due_range(cid, parameters, stability):
     else:
         easy_bonus = 1
     new_ivl = int(round(stability * easy_bonus * math.log(parameters['r']) / math.log(0.9)))
-    due = last_due + new_ivl
     if new_ivl <= 2.5:
         return (due, due), last_due
     last_elapsed_days = int((revlogs[0].time - revlogs[1].time) / 86400) if len(revlogs) >= 2 else 0
     min_ivl, max_ivl = get_fuzz_range(new_ivl, last_elapsed_days)
     due_range = (last_due + min_ivl, last_due + max_ivl)
+    if due > mw.col.sched.today:
+        due_range = tuple(map(lambda x: max(x, due), due_range))
     if due_range[1] < mw.col.sched.today:
         due_range = (due, due)
     return due_range, last_due
 
 def disperse(siblings):
-    due_ranges_last_due = {cid: get_due_range(cid, did_to_deck_parameters[did], stability) for cid, did, stability in siblings}
+    due_ranges_last_due = {cid: get_due_range(cid, did_to_deck_parameters[did], stability, due) for cid, did, stability, due in siblings}
     due_ranges = {cid: due_range for cid, (due_range, last_due) in due_ranges_last_due.items()}
     last_due = {cid: last_due for cid, (due_range, last_due) in due_ranges_last_due.items()}
     latest_due = max(last_due.values())
@@ -96,13 +98,14 @@ def disperse_siblings_backgroud(did, filter_flag=False, filtered_nid_string="", 
 
     card_cnt = 0
     note_cnt = 0
-    siblings = get_siblings(did, filter_flag, filtered_nid_string)
+    nid_siblings = get_siblings(did, filter_flag, filtered_nid_string)
+    sibilings_cnt = len(nid_siblings)
 
     mw.checkpoint("Siblings Dispersing")
-    mw.taskman.run_on_main(lambda: mw.progress.start(label="Siblings Dispersing", max=len(siblings), immediate=False))
+    mw.taskman.run_on_main(lambda: mw.progress.start(label="Siblings Dispersing", max=sibilings_cnt, immediate=False))
 
-    for nid, cards in siblings.items():
-        best_due_dates = disperse(cards)
+    for nid, siblings in nid_siblings.items():
+        best_due_dates = disperse(siblings)
         for cid, due in best_due_dates.items():
             card = mw.col.get_card(cid)
             last_revlog = mw.col.card_stats_data(cid).revlog[0]
@@ -113,7 +116,7 @@ def disperse_siblings_backgroud(did, filter_flag=False, filtered_nid_string="", 
         note_cnt += 1
 
         if note_cnt % 500 == 0:
-            mw.taskman.run_on_main(lambda: mw.progress.update(value=note_cnt, label=f"{note_cnt}/{len(siblings)} notes dispersed"))
+            mw.taskman.run_on_main(lambda: mw.progress.update(label=f"{note_cnt}/{len(nid_siblings)} notes dispersed", value=note_cnt, max=sibilings_cnt))
             if mw.progress.want_cancel(): break
             
     finished_text = f"{text_from_reschedule +', ' if text_from_reschedule != '' else ''}{card_cnt} cards in {note_cnt} notes dispersed."
