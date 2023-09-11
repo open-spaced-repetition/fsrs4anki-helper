0625ac71f5353d9faabced3c4211fdcf79ed7898
Luc Mcgrady <lucmcgrady@gmail.com>
2023-09-11 11:28:35 +0100

    Added: Scheduler downloader

--- schedule/update_scheduler.py
+++ schedule/update_scheduler.py
@@ -1,16 +1,26 @@
-from aqt.utils import askUser
+from aqt.utils import askUser, showInfo
 from aqt import mw
+from ..utils import get_version, geq_version
 
 import urllib.request
 
+SCHEDULER_URL = "https://raw.githubusercontent.com/open-spaced-repetition/fsrs4anki/main/fsrs4anki_scheduler.js"
+
+def get_internet_scheduler():
+    with urllib.request.urlopen(SCHEDULER_URL) as req:
+        return req.read().decode("UTF8")
+
 def update_scheduler(_):
     custom_scheduler = mw.col.get_config("cardStateCustomizer", None)
-    
-    if not custom_scheduler:
+    try:
+        version = get_version(custom_scheduler)
+    except IndexError:
         if askUser(
             "You dont appear to have the fsrs4anki scheduler set up\n"
             "Would you like to replace your custom scheduling code with the latest?"
         ):
-            pass
+            mw.col.set_config("cardStateCustomizer", get_internet_scheduler(), undoable=True)
+            showInfo("Successfully added scheduler, check it in the custom scheduling section of your deck config")
+            return
         else:
             return
