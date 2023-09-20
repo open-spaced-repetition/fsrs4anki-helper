b58d3a7d1d7288fbb8b9058cbb51f73adc148e21
Jarrett Ye <jarrett.ye@outlook.com>
2023-09-20 15:14:35 +0800

    not need custom scheduling in reset

--- schedule/reset.py
+++ schedule/reset.py
@@ -2,28 +2,12 @@ from ..utils import *
 
 
 def reset(did):
-    custom_scheduler = check_fsrs4anki(mw.col.all_config())
-    if custom_scheduler is None:
-        return
-    version = get_version(custom_scheduler)
-    if version[0] < 3:
-        showWarning("Require FSRS4Anki version >= 3.0.0")
-        return
-
-    deck_parameters = get_deck_parameters(custom_scheduler)
-    if deck_parameters is None:
-        return
-
     if not askUser(
         """Undo all changes made by rescheduling. 
     It will set the interval and due of all cards to the original value set when ratings (not the previous rescheduling).
     Are you sure?"""
     ):
         return
-
-    skip_decks = (
-        get_skip_decks(custom_scheduler) if geq_version(version, (3, 12, 0)) else []
-    )
 
     undo_entry = mw.col.add_custom_undo_entry("Reset")
 
@@ -34,13 +18,6 @@ def reset(did):
     reseted_cards = set()
     decks = sorted(mw.col.decks.all(), key=lambda item: item["name"], reverse=True)
     for deck in decks:
-        if any([deck["name"].startswith(i) for i in skip_decks]):
-            reseted_cards = reseted_cards.union(
-                mw.col.find_cards(
-                    f"\"deck:{deck['name']}\" \"is:review\"".replace("\\", "\\\\")
-                )
-            )
-            continue
         if did is not None:
             deck_name = mw.col.decks.get(did)["name"]
             if not deck["name"].startswith(deck_name):
