c17aae088b4f69cd310e21d144ae23ff57ba5e49
user1823 <92206575+user1823@users.noreply.github.com>
2023-09-28 09:36:31 +0530

    Memory states from SM-2 for incomplete review history (#248)
    Co-authored-by: Jarrett Ye <jarrett.ye@outlook.com>

--- schedule/reschedule.py
+++ schedule/reschedule.py
@@ -228,6 +228,23 @@ class FSRS:
     def set_card(self, card: Card):
         self.card = card
 
+    def memory_state_from_sm2(self, ease_factor, interval, requestretention):
+        if self.version[0] == 3:
+            stability = interval * math.log(0.9) / math.log(requestretention)
+            difficulty = 11.0 - (ease_factor - 1.0) / (
+                math.exp(self.w[6])
+                * math.pow(stability, self.w[7])
+                * (math.exp((1 - requestretention) * self.w[8]) - 1)
+            )
+        elif self.version[0] == 4:
+            stability = interval * math.log(0.9) / math.log(requestretention)
+            difficulty = 11.0 - (ease_factor - 1.0) / (
+                math.exp(self.w[8])
+                * math.pow(stability, -self.w[9])
+                * (math.exp((1 - requestretention) * self.w[10]) - 1)
+            )
+        return stability, difficulty
+
 
 def reschedule(
     did, recent=False, filter_flag=False, filtered_cids={}, filtered_nid_string=""
@@ -380,15 +397,24 @@ def reschedule_card(cid, fsrs: FSRS, rollover, params):
     revlogs = filter_revlogs(mw.col.card_stats_data(cid).revlog)
     reps = len(revlogs)
     for i, revlog in enumerate(reversed(revlogs)):
-        if (
-            i == 0
-            and (revlog.review_kind not in (REVLOG_LRN, REVLOG_RELRN))
-            and not (has_again(revlogs) or has_manual_reset(revlogs))
-        ):
-            break
-        last_s = s
         rating = revlog.button_chosen
-
+        if i == 0 and (
+            revlog.review_kind == REVLOG_REV
+            or (revlog.review_kind == REVLOG_CRAM and revlog.ease != 0)
+        ):
+            s, d = fsrs.memory_state_from_sm2(
+                revlog.ease / 1000, revlog.interval / 86400, retention
+            )
+            s = max(0.1, min(s, 36500))
+            d = constrain_difficulty(d)
+            again_s = s
+            hard_s = s
+            good_s = s
+            easy_s = s
+            last_date = datetime.fromtimestamp(revlog.time - rollover * 60 * 60)
+            last_rating = rating
+            continue
+        last_s = s
         if (
             last_kind is not None
             and last_kind in (REVLOG_REV, REVLOG_RELRN)
@@ -427,7 +453,7 @@ def reschedule_card(cid, fsrs: FSRS, rollover, params):
                 datetime.fromtimestamp(revlog.time - rollover * 60 * 60).toordinal()
                 - last_date.toordinal()
             )
-            if elapsed_days <= 0 and revlog.review_kind in (REVLOG_LRN, REVLOG_RELRN):
+            if elapsed_days <= 0:
                 continue
             r = (
                 exponential_forgetting_curve(elapsed_days, s)
