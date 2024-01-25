8fa741632eca05aef4601860a1856386d869d5f5
Jarrett Ye <jarrett.ye@outlook.com>
2024-01-25 17:57:17 +0800

    Feat/apply Easy Days to more than one date (#353)
    Co-authored-by: user1823 <92206575+user1823@users.noreply.github.com>

--- schedule/easy_days.py
+++ schedule/easy_days.py
@@ -2,6 +2,7 @@ from aqt import (
     QDate,
     QDateEdit,
     QDateTime,
+    QHBoxLayout,
     QLabel,
     QPushButton,
     QVBoxLayout,
@@ -61,31 +62,48 @@ def auto_easy_days():
 
 
 # Modified from https://github.com/sam1penny/countdown-to-events/blob/main/src/__init__.py#L86-L169
-class EasySpecificDateWidget(QWidget):
+class EasySpecificDateManagerWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.initUi()
+        self.specific_dates = []
 
     def initUi(self):
         self.layout = QVBoxLayout()
 
         self.dateLabel = QLabel()
-        self.dateLabel.setText("Select The Specific Date")
+        self.dateLabel.setText("Select the Date(s)")
         self.dateEdit = QDateEdit()
         self.dateEdit.setDateTime(QDateTime.currentDateTime())
-        self.dateEdit.setMinimumDate(QDate.currentDate())
 
-        self.applyEasyDayBtn = QPushButton("Apply Easy Day")
+        self.addDateBtn = QPushButton("Add the Selected Date")
+        self.addDateBtn.clicked.connect(self.addEventFunc)
+
+        self.applyEasyDayBtn = QPushButton("Apply Easy Days")
         self.applyEasyDayBtn.clicked.connect(self.apply_easy_day_for_specific_date)
 
         self.layout.addWidget(self.dateLabel)
         self.layout.addWidget(self.dateEdit)
+        self.layout.addWidget(self.addDateBtn)
         self.layout.addWidget(self.applyEasyDayBtn)
 
         self.layout.addStretch()
 
         self.setLayout(self.layout)
-        self.setWindowTitle("Easy Day for a specific date")
+        self.setWindowTitle("Easy Days for Specific Dates")
+
+    def addEventFunc(self):
+        specific_date = self.dateEdit.date().toPyDate()
+        if specific_date in self.specific_dates:
+            tooltip("This date has already been added")
+            return
+        if specific_date < datetime.now().date():
+            tooltip("Easy days can't be applied on past dates.")
+            return
+        self.specific_dates.append(specific_date)
+        deckWidget = DateLabelWidget(specific_date, self)
+        self.layout.insertWidget(self.layout.count() - 2, deckWidget)
+        mw.deckBrowser.refresh()
 
     def apply_easy_day_for_specific_date(self):
         config = Config()
@@ -93,12 +111,17 @@ class EasySpecificDateWidget(QWidget):
         if not config.load_balance:
             tooltip("Please enable load balance first")
             return
-        specific_date = self.dateEdit.date().toPyDate()
-        current_date = datetime.now().date()
-        day_offset = (specific_date - current_date).days
-        today = mw.col.sched.today
-        specific_due = today + day_offset
-        due_days = [specific_due]
+        if len(self.specific_dates) == 0:
+            tooltip("Please add the dates first.")
+            return
+        specific_dues = []
+        for specific_date in self.specific_dates:
+            current_date = datetime.now().date()
+            day_offset = (specific_date - current_date).days
+            today = mw.col.sched.today
+            specific_due = today + day_offset
+            specific_dues.append(specific_due)
+
         due_in_specific_date_cids = mw.col.db.list(
             f"""SELECT id
             FROM cards
@@ -107,7 +130,7 @@ class EasySpecificDateWidget(QWidget):
             AND CASE WHEN odid==0
             THEN due
             ELSE odue
-            END IN {ids2str(due_days)}
+            END IN {ids2str(specific_dues)}
             """
         )
 
@@ -116,10 +139,31 @@ class EasySpecificDateWidget(QWidget):
             recent=False,
             filter_flag=True,
             filtered_cids=set(due_in_specific_date_cids),
-            easy_specific_due_dates=[specific_due],
+            easy_specific_due_dates=specific_dues,
         )
 
 
+class DateLabelWidget(QWidget):
+    def __init__(self, date, manager):
+        super().__init__()
+        self.manager = manager
+        self.date = date
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+        self.eventDate = QLabel(date.strftime("%Y-%m-%d"))
+
+        self.deleteButton = QPushButton("Delete")
+        self.deleteButton.clicked.connect(self.deleteEvent)
+
+        layout.addWidget(self.eventDate)
+        layout.addWidget(self.deleteButton)
+
+    def deleteEvent(self):
+        self.manager.specific_dates.remove(self.date)
+        self.setParent(None)
+        mw.deckBrowser.refresh()
+
+
 def easy_day_for_sepcific_date(did):
-    mw.addEventWidget = EasySpecificDateWidget()
+    mw.addEventWidget = EasySpecificDateManagerWidget()
     mw.addEventWidget.show()
