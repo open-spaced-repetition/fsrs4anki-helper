7e72c7979f965f2caf19408f8386b29636a79f48
user1823 <92206575+user1823@users.noreply.github.com>
2024-01-25 09:02:45 +0530

    Disperse siblings - Create a new fuzz range for cards scheduled beyond max limit of fuzz

--- schedule/disperse_siblings.py
+++ schedule/disperse_siblings.py
@@ -102,8 +102,9 @@ def get_due_range(cid, stability, due, desired_retention, maximum_interval):
     )
     min_ivl, max_ivl = get_fuzz_range(new_ivl, last_elapsed_days, maximum_interval)
     if due > last_review + max_ivl:
-        due_range = (due, due)
-    elif due >= mw.col.sched.today:
+        # don't reschedule the card to bring it within the fuzz range. Rather, create another fuzz range around the original due date.
+        min_ivl, max_ivl = get_fuzz_range(due - last_review, last_elapsed_days, maximum_interval)
+    if due >= mw.col.sched.today:
         due_range = (
             max(last_review + min_ivl, mw.col.sched.today),
             max(last_review + max_ivl, mw.col.sched.today),
