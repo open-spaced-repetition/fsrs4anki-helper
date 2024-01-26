ac0f125f671be67294208fc9e242b16dfb04bf6e
user1823 <92206575+user1823@users.noreply.github.com>
2024-01-26 12:53:53 +0530

    Clarify comment

--- schedule/disperse_siblings.py
+++ schedule/disperse_siblings.py
@@ -104,7 +104,8 @@ def get_due_range(cid, stability, due, desired_retention, maximum_interval):
     if due > last_review + max_ivl + 2: # +2 is just a safeguard to exclude cards that go beyond the fuzz range due to rounding 
         # don't reschedule the card to bring it within the fuzz range. Rather, create another fuzz range around the original due date.
         current_ivl = due - last_review
-        min_ivl, max_ivl = get_fuzz_range(current_ivl, last_elapsed_days, current_ivl) # set max_ivl = current_ivl to prevent a further increase in ivl
+        # set maximum_interval = current_ivl to prevent a further increase in ivl
+        min_ivl, max_ivl = get_fuzz_range(current_ivl, last_elapsed_days, current_ivl)
     if due >= mw.col.sched.today:
         due_range = (
             max(last_review + min_ivl, mw.col.sched.today),
