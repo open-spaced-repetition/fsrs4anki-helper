9c44a2edeabded9242919e386e4d4a8b2daebd27
Jarrett Ye <jarrett.ye@outlook.com>
2024-04-15 17:54:31 +0800

    update Interpretation

--- stats.py
+++ stats.py
@@ -144,7 +144,7 @@ def get_fsrs_stats(self: CollectionStats):
         _line_now(
             i,
             "Knowledge acquisition per hour",
-            f"{estimated_total_knowledge / (time_sum/3600):.2f} cards",
+            f"{estimated_total_knowledge / (time_sum/3600):.1f} cards",
         )
     i.append(
         "<tr><td align=left style='padding: 5px'><b>Retention by Notes:</b></td></tr>"
@@ -169,6 +169,7 @@ def get_fsrs_stats(self: CollectionStats):
         + "<li><b>Load</b>: an estimate of the average number of cards to be reviewed daily (assuming review at the scheduled time without advancing or postponing). Load = 1/I<sub>1</sub> + 1/I<sub>2</sub> + 1/I<sub>3</sub> +...+ 1/I<sub>n</sub>, where I<sub>n</sub> is the current interval of the n-th card.</li>"
         + "<li><b>Count</b>: the number of cards with FSRS memory states, excluding cards in the (re)learning stage.</li> "
         + "<li><b>Estimated total knowledge</b>: the number of cards the user is expected to know today, calculated as the product of average retention and count.</li>"
+        + "<li><b>Knowledge acquisition rate</b>: the number of cards memorized per hour of actively doing reviews in Anki, calculated as the ratio of total knowledge and total time. Larger values indicate efficient learning. This metric can be used to compare different learners. If your collection is very young, this number may initially be very low or very high.</li>"
         + "</ul></details>"
     )
     return self._section(
