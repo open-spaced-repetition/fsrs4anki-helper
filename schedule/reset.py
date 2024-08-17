ab1f8f734f1c28191600274b25fcc4d11713a038
Jarrett Ye <jarrett.ye@outlook.com>
2024-08-17 16:55:08 +0800

    Feat/show the change of target retention (#445)

--- schedule/reset.py
+++ schedule/reset.py
@@ -20,14 +20,16 @@ def clear_custom_data(did):
     )
 
     cnt = 0
+    reset_cards = []
     start_time = time.time()
     undo_entry = mw.col.add_custom_undo_entry("Clear custom data")
     for cid in cards:
         card = mw.col.get_card(cid)
         card.custom_data = ""
+        reset_cards.append(card)
         cnt += 1
 
-    mw.col.update_cards(cards)
+    mw.col.update_cards(reset_cards)
     mw.col.merge_undo_entries(undo_entry)
     tooltip(f"""{cnt} cards cleared in {time.time() - start_time:.2f} seconds.""")
     mw.reset()
