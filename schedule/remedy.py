d652536eea607569018c98715d2807294d2c61cf
Jarrett Ye <jarrett.ye@outlook.com>
2024-09-15 17:49:08 +0800

    replace text input with QDateEdit

--- schedule/remedy.py
+++ schedule/remedy.py
@@ -2,79 +2,86 @@ import os
 from pathlib import Path
 from ..utils import *
 from anki.utils import ids2str
+from aqt import QDateTime, QWidget, QVBoxLayout, QLabel, QPushButton, QDateEdit
 
 
-def RepresentsDatetime(s):
-    try:
-        return datetime.strptime(s, "%Y%m%d")
-    except ValueError:
-        tooltip("Invalid date format")
-        return None
+class RemedyDateRangeWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setup_ui()
 
+    def setup_ui(self):
+        self.layout = QVBoxLayout()
 
-def ask_date_range():
-    (s, r) = getText("Enter the start date in the format YYYYMMDD")
-    if not r:
-        return None, None
-    start_date = RepresentsDatetime(s)
-    if not start_date:
-        return None, None
-    (s, r) = getText("Enter the end date in the format YYYYMMDD")
-    if not r:
-        return None, None
-    end_date = RepresentsDatetime(s)
-    if not end_date:
-        return None, None
-    return start_date, end_date
+        self.start_date_label = QLabel("Select the Start Date")
+        self.start_date_edit = QDateEdit()
+        self.start_date_edit.setDateTime(datetime.strptime("2006-01-01", "%Y-%m-%d"))
+        self.end_date_label = QLabel("Select the End Date")
+        self.end_date_edit = QDateEdit()
+        self.end_date_edit.setDateTime(QDateTime.currentDateTime())
+        self.remedy_button = QPushButton("Remedy")
+        self.remedy_button.clicked.connect(self.remedy_hard_misuse)
+
+        self.layout.addWidget(self.start_date_label)
+        self.layout.addWidget(self.start_date_edit)
+        self.layout.addWidget(self.end_date_label)
+        self.layout.addWidget(self.end_date_edit)
+        self.layout.addWidget(self.remedy_button)
+
+        self.setLayout(self.layout)
+        self.setWindowTitle("Remedy Hard Misuse")
+        self.resize(300, 200)
+
+    def remedy_hard_misuse(self):
+        revlog_ids = mw.col.db.list(
+            f"""SELECT id
+            FROM revlog
+            WHERE ease = 2
+            AND id > {self.start_date_edit.dateTime().toMSecsSinceEpoch()}
+            AND id < {self.end_date_edit.dateTime().toMSecsSinceEpoch()}
+            """
+        )
+
+        if len(revlog_ids) == 0:
+            tooltip(
+                "There are no reviews with a Hard rating in the selected range of dates."
+            )
+            return
+
+        start_date = self.start_date_edit.date().toString("yyyy-MM-dd")
+        end_date = self.end_date_edit.date().toString("yyyy-MM-dd")
+        yes = askUser(
+            f"""Between {start_date} and {end_date}, {len(revlog_ids)} reviews had a Hard rating.
+These ratings will be replaced with Again. The IDs of these revlogs will be stored in a CSV file in the addon folder to allow undoing the changes.
+Do you want to proceed?
+        """
+        )
+
+        if not yes:
+            return
+
+        mw.col.db.execute(
+            f"""UPDATE revlog
+            SET ease = 1, usn = -1
+            WHERE id IN {ids2str(revlog_ids)}
+            """
+        )
+        col_set_modified()
+
+        addon = mw.addonManager.addonFromModule(__name__)
+        user_files = Path(mw.addonManager.addonsFolder(addon)) / "user_files"
+        user_files.mkdir(parents=True, exist_ok=True)
+        revlog_id_csv = os.path.join(user_files, f"{mw.pm.name}_hard_misuse_remedy.csv")
+        with open(revlog_id_csv, "a") as f:
+            f.write("\n".join(map(str, revlog_ids)))
+
+        tooltip(f"{len(revlog_ids)} reviews remedied")
+        mw.reset()
 
 
 def remedy_hard_misuse(did):
-    start_date, end_date = ask_date_range()
-    if not start_date or not end_date:
-        return
-
-    revlog_ids = mw.col.db.list(
-        f"""SELECT id
-        FROM revlog
-        WHERE ease = 2
-        AND id > {start_date.timestamp() * 1000}
-        AND id < {end_date.timestamp() * 1000}
-        """
-    )
-
-    if len(revlog_ids) == 0:
-        tooltip(
-            "There are no reviews with a Hard rating in the selected range of dates."
-        )
-        return
-
-    yes = askUser(
-        f"""Between {start_date.strftime("%Y-%m-%d")} and {end_date.strftime("%Y-%m-%d")}, {len(revlog_ids)} reviews had a Hard rating.
-These ratings will be replaced with Again. The IDs of these revlogs will be stored in a CSV file in the addon folder to allow undoing the changes.
-Do you want to proceed?
-    """
-    )
-
-    if not yes:
-        return
-
-    mw.col.db.execute(
-        f"""UPDATE revlog
-        SET ease = 1, usn = -1
-        WHERE id IN {ids2str(revlog_ids)}
-        """
-    )
-    col_set_modified()
-
-    addon = mw.addonManager.addonFromModule(__name__)
-    user_files = Path(mw.addonManager.addonsFolder(addon)) / "user_files"
-    user_files.mkdir(parents=True, exist_ok=True)
-    revlog_id_csv = os.path.join(user_files, f"{mw.pm.name}_hard_misuse_remedy.csv")
-    with open(revlog_id_csv, "a") as f:
-        f.write("\n".join(map(str, revlog_ids)))
-
-    tooltip(f"{len(revlog_ids)} reviews remedied")
-    mw.reset()
+    mw.date_range_widget = RemedyDateRangeWidget()
+    mw.date_range_widget.show()
 
 
 def undo_remedy(did):
