f2c15cd1a191927b9cf011cb1cfc9b41050d654d
Jarrett Ye <jarrett.ye@outlook.com>
2024-10-29 10:53:05 +0800

    hide Daily Load if Anki version >= 24.10 (#483)

--- stats.py
+++ stats.py
@@ -128,7 +128,8 @@ def get_fsrs_stats(self: CollectionStats):
     i = []
     _line_now(i, "Average predicted retention", f"{retention * 100: .2f}%")
     _line_now(i, "Average stability", f"{round(stability)} days")
-    _line_now(i, "Daily Load", f"{round(load)} reviews/day")
+    if int_version() < 241000:
+        _line_now(i, "Daily Load", f"{round(load)} reviews/day")
     i.append(
         "<tr><td align=left style='padding: 5px'><b>Retention by Cards:</b></td></tr>"
     )
