3b7d8884af05fdc518e31f0d96835dba06111e19
Jarrett Ye <jarrett.ye@outlook.com>
2025-05-26 18:56:47 +0800

    Fix/use card's decay when calling next interval

--- schedule/disperse_siblings.py
+++ schedule/disperse_siblings.py
@@ -91,7 +91,7 @@ def get_siblings_when_review(card: Card):
 def get_due_range(cid, stability, due, desired_retention, maximum_interval):
     card = mw.col.get_card(cid)
     last_review = get_last_review_date(card)
-    new_ivl = next_interval(stability, desired_retention)
+    new_ivl = next_interval(stability, desired_retention, -get_decay(card))
 
     if new_ivl <= 2.5:
         return (due, due), last_review
