6078218218d5456dc0f80c089572184e035cf257
Luc Mcgrady <lucmcgrady@gmail.com>
2025-11-17 06:26:51 +0000

    Fix/Map de to de_DE (#637)

--- i18n.py
+++ i18n.py
@@ -7,6 +7,8 @@ from aqt.utils import tr
 locale = mw.pm.meta["defaultLang"]
 if locale == "en":
     locale = "en_US"
+elif locale == "de":
+    locale = "de_DE"
 
 addon_dir = Path(os.path.dirname(__file__))
 
